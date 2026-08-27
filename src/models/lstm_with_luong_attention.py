6a62e749234d660f0ccfd79298ccd430307e792e
AxelF23 <dima-ego-2003@mail.ru>
2026-08-27 15:49:59 +0300

    Update lstm_with_luong_attention.py

--- src/models/lstm_with_luong_attention.py
+++ src/models/lstm_with_luong_attention.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class Encoder_att(nn.Module):
   """
   LSTM - based encoder
@@ -75,3 +79,42 @@ class Decoder_att(nn.Module):
 
         prediction = self.fc_out(attentional_hidden.squeeze(0))
         return prediction, hidden, cell
+
+class Seq2Seq_att(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(
+            trg_len,
+            batch_size,
+            trg_vocab_size
+        ).to(self.device)
+
+        encoder_outputs, hidden, cell = self.encoder(src)
+
+        input_token = trg[0]
+
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(
+                input_token,
+                encoder_outputs,
+                hidden,
+                cell
+            )
+
+            outputs[t] = output
+
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+
+            input_token = trg[t] if teacher_force else top1
+
+        return outputs
